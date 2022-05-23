82fd6e4956e6f60fdf407085c2378986b390bc8c
rfant <105244434+rfant@users.noreply.github.com>
2022-05-23 07:33:56 -0500

    Add files via upload
    This decides whether to use the production postgresql database, a pre-production version or a VM version.

---
+++ CMVP_Module_Tracker/dev_or_prod.h
@@ -0,0 +1,18 @@
+//Parser: This defines which postgresql database the parser will insert the new data
+//Intel intranet
+
+//comment out one of these lines. 
+
+// ubuntu local Virtual Machine
+//#define PROD 2
+
+//intel intranet prodution 
+//#define PROD 1
+
+//intel internet pre-prod development mode
+#define PROD  0
+
+unsigned char VMencryptedPW []={218,25,112,80,80,4,144,228,28,211,51,153,143,165,170,231};
+unsigned char IntelencryptedPW []={44,155,95,216,43,67,13,219,62,86,123,205,185,227,198,171};
+
+	
