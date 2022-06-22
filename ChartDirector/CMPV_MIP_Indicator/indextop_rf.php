0f0a1bf609fc1b149b5d0f83229d1703644fa3ea
rfant <105244434+rfant@users.noreply.github.com>
2022-06-22 09:25:54 -0500

    Delete ChartDirector/CMPV_MIP_Indicator directory

--- ChartDirector/CMPV_MIP_Indicator/indextop_rf.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body style="background:#000080;margin:0px">
-<div style="margin-left:1; font-size:9pt; font-family:verdana; font-weight:bold; color:#FFFF00;"><i>atsec information security corporation</i></div>
-</body>
-</html>
--- ChartDirector/CMPV_MIP_Indicator/temp_score_sql.sql
+++
@@ -1,8 +0,0 @@
-select "Clean_Lab_Name",
-TRUNC(AVG(abs("Finalization_Start_Date" -"In_Review_Start_Date")) )
-as DaysInMip,
-trunc(avg(abs("Finalization_Start_Date" - "Coordination_Start_Date"))) as DaysInCO
-from "CMVP_MIP_Table" where "Review_Pending_Start_Date" between '2020-08-01' and '2022-01-30' 
-and ("Status2" like '%Promoted%' OR "Status2" like '%Reappear%' OR "Status2" is null) 
-and 	("In_Review_Start_Date" is not null AND  "Finalization_Start_Date" is not null)
-Group by "Clean_Lab_Name" order by daysinmip 
