4b02648ac09de761a437e6ad411e144ef5dcbfaf
rfant <105244434+rfant@users.noreply.github.com>
2022-09-26 11:34:26 -0500

    added userKey_ for aes

--- CMVP_Module_Tracker/dev_or_prod.h
+++ CMVP_Module_Tracker/dev_or_prod.h
@@ -4,15 +4,14 @@
 //comment out one of these lines. 
 
 // ubuntu local Virtual Machine
-//#define PROD 2
+#define PROD 2
 
 //intel intranet prodution 
 //#define PROD 1
 
 //intel internet pre-prod development mode
-#define PROD  0
+//#define PROD  0
 
 unsigned char VMencryptedPW []={218,25,112,80,80,4,144,228,28,211,51,153,143,165,170,231};
 unsigned char IntelencryptedPW []={44,155,95,216,43,67,13,219,62,86,123,205,185,227,198,171};
-
-	
+unsigned char userKey_[]="0003141592653598";
