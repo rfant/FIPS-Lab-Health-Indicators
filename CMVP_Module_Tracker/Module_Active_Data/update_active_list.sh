ad7637f040634e05187d3e75bd4a9921ba9a74e5
rfant <105244434+rfant@users.noreply.github.com>
2022-10-27 08:32:00 -0500

    Updated SMB external drive path name

--- CMVP_Module_Tracker/Module_Active_Data/update_active_list.sh
+++ CMVP_Module_Tracker/Module_Active_Data/update_active_list.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #used to get the latest Active Module  list from CMVP
 #this will include cert_numbers, vendor, module name, lab name, sunset, etc.
 
 
-MY_TOOL_PATH="/home/rfant/CMVP_Module_Tracker/Module_Active_Data"
-MY_LOCAL_PATH="/home/rfant/CMVP_Module_Tracker/Module_Active_Data/active_cert_pull"
 
+MY_TOOL_PATH="/home/vcap/app/.bp-config/Module_Active_Data"
+MY_LOCAL_PATH="/home/vcap/app/.bp-config/Module_Active_Data/active_cert_pull"
+MY_BACKUP_PATH="/var/vcap/data/LHI/Module_Active_Data"
 
 today=`date '+%m_%d_%Y'`;
 LOG_FILENAME="$MY_TOOL_PATH/results/$today.CMVP_pull_of_Active_Modules.log"
@@ -15,13 +16,8 @@ KILL_FILENAME= "$MY_LOCAL_PATH/kill.txt"
 
 echo "LOG_FILENAME=$LOG_FILENAME"
 rm -f $LOG_FILENAME
-touch $LOG_FILENAME
+date > $LOG_FILENAME
 
-
-#echo "Disconnect VPN:  " >>$LOG_FILENAME
-#osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "disconnect \"RichardFant2020_ext\"" -e "end tell"
-#give 5 seconds to disconnect
-#sleep 5
 
 echo "Updating my Active Module List (reading ALL the current & historic Certs, each Cert with its own individual HTML file)." >> $LOG_FILENAME
 echo "cd $MY_LOCAL_PATH" >> $LOG_FILENAME
@@ -30,8 +26,8 @@ cd $MY_LOCAL_PATH
 
 #Instead of pulling the Active & Historic certs seperately, I will instead  pull all certs sequentially from 1 to 6000. 
 #Some of these cert numbers are bogus since the number was skipped or is in the future.
-#But, this way, I get all Active and Historic and Revoked certificates in one fell swoop.
-#I'll then clean up all dupe cert files and delete any bogus cert files.
+#But, this way, I get all Active and Historic and Revoked certificates in one fell swoop. Otherwise I'd need two separate pulls.
+
 rm -r $URL_FILENAME
 
 for i in {1..6000}  #fix up my sequential cert list. see above comment.
@@ -47,30 +43,24 @@ cd $MY_LOCAL_PATH
 xargs -n 1 curl -O < $URL_FILENAME
 
 #-------------------------------------------------------------------
-#delete all "Page Not Found" files since they are bogus cert files.
-#grep -lrIZ 'Page Not Found' . > $KILL_FILENAME
-#cat $KILL_FILENAME
-#echo "Delete those bogus certifications " 
-#while read file; do rm -f "$file"; done < $KILL_FILENAME
+# Backup all my Active Cert data to the Network Share Drive
+cp $MY_TOOL_PATH/active_cert_pull/* $MY_BACKUP_PATH/active_cert_pull_backup/.
+
+
+
+#----------------
+# now parse all the files 
+echo "Finished CMVP Active_cert_pull">> $LOG_FILENAME
+echo "launching  HTML parser for ALL these thousands of Certs HTML files." >> $LOG_FILENAME
+#echo pwd >> $LOG_FILENAME
+echo " ./go" >> $LOG_FILENAME
+
+# Backup all my log files to the Network Share Drive
+cp $MY_TOOL_PATH/results/* $MY_BACKUP_PATH/results/.
+
 
 cd $MY_TOOL_PATH
-
-
-#-------------------
-#delete all dups that may occur from pulling duplicate certifcate files on different dates.
-#echo "Find all dupes and delete: fdupes -dN /Users/fant/CMVP_Module_Tracker/Module_Active_Data/active_cert_pull">> $LOG_FILENAME
-#fdupes -dN /Users/fant/CMVP_Module_Tracker/Module_Active_Data/active_cert_pull >> $LOG_FILENAME
-#----------------
-# now parse the all the files 
-echo "Finished CMVP Active_cert_pull">> $LOG_FILENAME
-
-
-
-echo "launching  HTML parser for ALL these thousands of Certs HTML files." >> $LOG_FILENAME
-echo "     ./go" >> $LOG_FILENAME
 ./go
-
-
 
 
 
