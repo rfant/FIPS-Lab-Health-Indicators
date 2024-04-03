2258526a0f6b4088428d032ef0e9e48826c258c8
rfant <105244434+rfant@users.noreply.github.com>
2024-04-03 15:17:12 -0500

    Updates due to Checkmarx

--- CMVP_Module_Tracker/Module_In_Process_Data/setMIPErrorFlag.cpp
+++ CMVP_Module_Tracker/Module_In_Process_Data/setMIPErrorFlag.cpp
@@ -84,8 +84,8 @@ int main (int argc, char* argv[]) {
 
 	switch (PROD) {
 		case 2:  			//local VM machine
-			AES_set_decrypt_key(userKey_, 128, &aesKey_);
-    		AES_decrypt(VMencryptedPW, decryptedPW,&aesKey_);
+			//AES_set_decrypt_key(userKey_, 128, &aesKey_);
+    		//AES_decrypt(VMencryptedPW, decryptedPW,&aesKey_);
     		
     		snprintf(connbuff,sizeof connbuff,"host=localhost user=postgres password=%s dbname=postgres", decryptedPW);
        		conn = PQconnectdb(connbuff);
@@ -95,19 +95,20 @@ int main (int argc, char* argv[]) {
 		case 1: 			//intel intranet production
   		
 	  		
-			AES_set_decrypt_key(userKey_, 128, &aesKey_);
-    		AES_decrypt(IntelencryptedPW, decryptedPW,&aesKey_);
+			//AES_set_decrypt_key(userKey_, 128, &aesKey_);
+    		//AES_decrypt(IntelencryptedPW, decryptedPW,&aesKey_);
 
-			
-    		snprintf(connbuff,sizeof connbuff,"host=postgres5320-lb-fm-in.dbaas.intel.com user=lhi_prod2_so password=%s dbname=lhi_prod2 ", decryptedPW);
+			//rgf2
+    		//snprintf(connbuff,sizeof connbuff,"host=postgres5320-lb-fm-in.dbaas.intel.com user=lhi_prod2_so password=%s dbname=lhi_prod2 ", decryptedPW);
+    		snprintf(connbuff,sizeof connbuff,"host=postgres5320-lb-fm-in.dbaas.intel.com user=lhi_prod2_so password=%s dbname=lhi_prod2 ", encryptedPW);
     
     		conn = PQconnectdb(connbuff);
    	   		break;
 	
 		case 0: //Intel intranet pre-production
 			
-		 	AES_set_decrypt_key(userKey_, 128, &aesKey_);
-    		AES_decrypt(IntelencryptedPW, decryptedPW,&aesKey_);
+		 	//AES_set_decrypt_key(userKey_, 128, &aesKey_);
+    		//AES_decrypt(IntelencryptedPW, decryptedPW,&aesKey_);
     		
     		snprintf(connbuff,sizeof connbuff,"host=postgres5596-lb-fm-in.dbaas.intel.com user=lhi_pre_prod_so password=%s dbname=lhi_pre_prod ", decryptedPW);
     
@@ -124,7 +125,7 @@ int main (int argc, char* argv[]) {
     if (PQstatus(conn) == CONNECTION_OK) {
 
 
-    	//clear the Active Error Flag
+    	//clear the MIP Error Flag
 		CLR_SQL1_STR
 		
 		//get the Error Log file name
@@ -150,7 +151,7 @@ int main (int argc, char* argv[]) {
 		sql_result = PQexec(conn, sql1); 
 
 		if (PQresultStatus(sql_result) != PGRES_COMMAND_OK)  
-			printf("\nError 134: SQL Set Active Error Flag Failed: sql1=%s\n",sql1);
+			printf("\nError 134: SQL Set MIP Error Flag Failed: sql1=%s\n",sql1);
 		PQclear(sql_result);
 			
 
